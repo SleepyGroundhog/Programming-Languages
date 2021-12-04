3302f16909162a130e8dd539c3005a4a0e8fa80c
Nikita <nik.vasilenko1203@gmail.com>
2021-12-04 18:28:50 +0300

    Branch init

--- List.h
+++ List.h
@@ -1,50 +1,118 @@
 #pragma once
-#include <initializer_list>
 
-template <class value_t> class List;
-
-template <class value_t> class List {
+template <class value_t>
+class List {
+private:
+	class Node;
 public:
-	class Node {
-		friend List<value_t>;
-		friend List<value_t>::Iterator;
-	public:
-		value_t        m_value;
-		Node* m_next;
-		Node* m_prev;
-		Node(value_t value) : Node() { m_value = value; }
-	private:
-		Node() : m_next(0), m_prev(0) {}
-	};
-
 	class Iterator;           
 
-	List();
-	List(const List&);
-	List(const std::initializer_list<value_t>&);
-	List(const List<value_t>::Iterator&, const List<value_t>::Iterator&);
-	
-	~List();
+	List() : tail_next(new Node), head(tail_next), tail(tail_next), count(0) {}
+	List(const List& list) : List() { *this = list; }
+	List(const List::Iterator&, const List::Iterator&);
+    ~List() { clear(); delete tail_next; }
 
 	List& operator=(const List&);
 
-	size_t size() const;
-	void   clear();
+	bool isEmpty() { return count == 0; }
+	
+	size_t size() const { return count; }
+
+	void clear() { while ( !isEmpty() ) pop_back(); }
 
 	void pop_back();         
 	void push_back(value_t); 
-	void print(bool = true); 	 
+	void print(bool newline = true); 	 
 
-	Iterator begin() const;
-	Iterator end() const;
+	Iterator begin() const { return Iterator(head); }
+	Iterator end() const { tail_next->prev = tail; return Iterator(tail_next); }
 
 private:
-	Node*   m_first;    
-	Node*   m_last;     
-	Node*   m_end;      
-	size_t  m_size;  
+	Node*   head;    
+	Node*   tail;     
+	size_t  count;  // used for counting list nodes
+
+	// fictitious elements
+	Node* tail_next;
+  //Node* first_prev;
+
+	class Node {
+		friend List;
+		friend List::Iterator;
+
+	public:
+		value_t  value;
+		Node*    next;
+		Node*    prev;
+		Node(value_t value) : Node() { this->value = value; }
+		Node() : next(0), prev(0) {}
+
+	};
 	
 };
 
 #include "List.inl"
 
+template <class value_t> class List<value_t>::Iterator {
+	friend class List;
+public:
+
+	Iterator(const Iterator& iterator) { *this = iterator; }
+	Iterator& operator=(const Iterator& iterator) { if (this != &iterator) { it = iterator.it; } return *this; }
+
+	Iterator& operator++() {
+		if (it->next == 0) { // если it указывает на фиктивный элемент
+			throw (std::logic_error("list iterator out of range"));
+		}
+		it = it->next;
+		return *this;
+	}
+
+	Iterator  operator++(int) {
+		List<value_t>::Iterator temp(*this);
+		++(*this);
+		return temp;
+	}
+
+	Iterator& operator--() {
+		if (it->prev == 0) { // если it указывает на первый элемент
+			throw (std::logic_error("list iterator out of range"));
+		}
+		it = it->prev;
+		return *this;
+	}
+
+	Iterator  operator--(int) {
+		List<value_t>::Iterator temp(*this);
+		--(*this);
+		return temp;
+	}
+
+	Iterator  operator+(size_t count) {
+		List<value_t>::Iterator temp = *this;
+		for (int i = 0; i < count; ++i)
+			++temp;
+		return temp;
+	}
+
+	Iterator  operator-(size_t count) {
+		List<value_t>::Iterator temp = *this;
+		for (int i = 0; i < count; ++i)
+			--temp;
+		return temp;
+	}
+
+	Iterator& operator+=(size_t count) { *this = *this + count; return *this; }
+	Iterator& operator-=(size_t count) { *this = *this - count; return *this; }
+	value_t& operator*() { return it->value; }
+
+	bool operator!=(const List<value_t>::Iterator& right) const {
+		if (it == 0 || right.it == 0)
+			return true;
+		return it != right.it;
+	}
+
+private:
+	Iterator(Node* node) { it = node; }
+	Node* it;
+};
